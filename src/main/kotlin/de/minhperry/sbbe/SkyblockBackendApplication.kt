ad322860e55470170f5c93b6be8efe890a9cab7a
minhperry <minhpt2002@gmail.com>
2025-06-01 16:52:02 +0200

    add caching

--- src/main/kotlin/de/minhperry/sbbe/SkyblockBackendApplication.kt
+++ src/main/kotlin/de/minhperry/sbbe/SkyblockBackendApplication.kt
@@ -2,9 +2,12 @@ package de.minhperry.sbbe
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class SkyblockBackendApplication
+// TODO: with ttl https://www.baeldung.com/spring-setting-ttl-value-cache
 
 fun main(args: Array<String>) {
 	runApplication<SkyblockBackendApplication>(*args)
