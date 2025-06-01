e6c96344a194cc521830febe4341096620d9481c
minhperry <minhpt2002@gmail.com>
2025-06-01 13:52:41 +0200

    a mojang player may have a hypixel player entry

--- src/main/kotlin/de/minhperry/sbbe/entity/MojangPlayer.kt
+++ src/main/kotlin/de/minhperry/sbbe/entity/MojangPlayer.kt
@@ -1,10 +1,12 @@
 package de.minhperry.sbbe.entity
 
 import de.minhperry.sbbe.entity.dto.MojangPlayerDTO
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
 import jakarta.persistence.Id
+import jakarta.persistence.OneToOne
 import jakarta.validation.constraints.Size
 import java.util.UUID
 
@@ -12,22 +14,28 @@ import java.util.UUID
 @Table(
     name = "player",
 )
-data class MojangPlayer(
-    @Id
-    val id: String,
-
+class MojangPlayer(
     @Column(nullable = false, length = 16, name = "player_name")
-    @Size(max = 16) // Minecraft name can only contain at most 16 chars
+    @Size(max = 16, message = "Minecraft name can only contain at most 16 chars")
     val name: String,
 
+    @Id()
     @Column(nullable = false, name = "player_uuid")
     val uuid: UUID,
+
+    @OneToOne(
+        mappedBy = "mojangPlayer",
+        cascade = [CascadeType.ALL],
+        orphanRemoval = true,
+        optional = false
+    )
+    val hypixelPlayer: HypixelPlayer? = null
 ) {
     fun asDTO(): MojangPlayerDTO {
         return MojangPlayerDTO(this.uuid, this.name)
     }
 
     override fun toString(): String {
-        return "Player(id='$id', uuid=$uuid, name='$name')"
+        return "Player(uuid=$uuid, name='$name')"
     }
 }
