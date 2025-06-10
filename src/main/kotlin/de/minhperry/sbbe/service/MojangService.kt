bfcfab8f5c7b6ea4cd654c005b30d635f47a294b
minhperry <minhpt2002@gmail.com>
2025-06-10 22:28:14 +0200

    add save by uuid

--- src/main/kotlin/de/minhperry/sbbe/service/MojangService.kt
+++ src/main/kotlin/de/minhperry/sbbe/service/MojangService.kt
@@ -76,6 +76,35 @@ class MojangService(
         }
     }
 
+    fun saveByUuid(uuid: UUID): MojangPlayer? {
+        val url = "https://api.minecraftservices.com/minecraft/profile/lookup/uuid/$uuid"
+        return try {
+            val resp = restTemplate.getForObject(url, MojangSuccessResponse::class.java)
+            if (resp != null) {
+                // Save in database as lowercase name
+                val player = MojangPlayer(
+                    resp.name.lowercase(),
+                    uuid = uuid,
+                    playerExists = true
+                )
+                mojangPlayerRepository.save(player)
+            } else {
+                logger.error("Received null response from Mojang API for UUID '$uuid'")
+                null
+            }
+        } catch (e: HttpClientErrorException.NotFound) {
+            logger.error("Player with UUID '$uuid' not found in Mojang API", e)
+            mojangPlayerRepository.save(
+                MojangPlayer(
+                    name = "$$\$Unknown$$$",
+                    uuid = uuid,
+                    playerExists = false
+                )
+            )
+            null
+        }
+    }
+
     data class MojangSuccessResponse(
         val id: String,
         val name: String
