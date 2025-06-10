cae472d1ee976a021e1318c239055fcb238295ea
minhperry <minhpt2002@gmail.com>
2025-06-10 21:29:01 +0200

    move exception controller out

--- src/main/kotlin/de/minhperry/sbbe/entity/BaseEntity.kt
+++ src/main/kotlin/de/minhperry/sbbe/entity/BaseEntity.kt
@@ -2,17 +2,18 @@ package de.minhperry.sbbe.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
 import java.io.Serializable
 import java.util.Objects
 
 @MappedSuperclass
-class BaseEntity(
+abstract class BaseEntity(
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false, updatable = false)
-    private val id: Long? = null
+    var id: Long? = null
 ) : Serializable {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
