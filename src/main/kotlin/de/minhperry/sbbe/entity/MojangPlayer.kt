621479df4e6445ddda6e6a8443d4f235b41c6318
minhperry <minhpt2002@gmail.com>
2025-06-10 21:29:57 +0200

    add exist and last modified column for caching

--- src/main/kotlin/de/minhperry/sbbe/entity/MojangPlayer.kt
+++ src/main/kotlin/de/minhperry/sbbe/entity/MojangPlayer.kt
@@ -4,14 +4,19 @@ import de.minhperry.sbbe.entity.dto.MojangPlayerDTO
 import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Table
-import jakarta.persistence.Id
 import jakarta.persistence.OneToOne
 import jakarta.validation.constraints.Size
+import java.time.Instant
 import java.util.UUID
 
+/**
+ * Represents a Minecraft player as stored in the Mojang API.
+ * @param name The player's Minecraft name, which can be at most 16 characters long.
+ * @param uuid The player's unique identifier (UUID).
+ * @param playerExists Indicates whether the player exists in the Mojang API.
+ * @param lastModified The last time this player was modified in the database.
+ */
 @Entity
 @Table(
     name = "player",
@@ -21,8 +26,13 @@ class MojangPlayer(
     @Size(max = 16, message = "Minecraft name can only contain at most 16 chars")
     val name: String,
 
-    @Column(name = "player_uuid", nullable = false, unique = true)
+    @Column(name = "player_uuid", nullable = false, unique = true, columnDefinition = "uuid")
     val uuid: UUID,
+
+    @Column(name = "exist", nullable = false)
+    var playerExists: Boolean = false,
+
+    var lastModified: Instant = Instant.now(),
 
     @OneToOne(
         mappedBy = "mojangPlayer",
